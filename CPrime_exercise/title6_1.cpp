385e0a8445b20d08e6e047ad8aaf24555e5df748
Yx-hust <1454724668@qq.com>
2021-02-25 23:00:55 +0800

    Chapter exercise

---
+++ CPrime_exercise/title6_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cctype>
+int main()
+{
+    using namespace std;
+    char ch;
+    while((ch=cin.get())!='@')
+    {
+        if(isupper(ch))
+          ch=tolower(ch);
+        else if(islower(ch))
+          ch=toupper(ch);
+        cout<<ch;
+    }
+    return 0;
+}
